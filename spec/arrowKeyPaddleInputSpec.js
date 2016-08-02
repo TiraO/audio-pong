3b6474de5aa9a4f9683db104c96f739f7829e32e
Tira Odhner <atiraodhner@gmail.com>
2016-08-01 22:33:54 -0400

    WIP holding down the arrow key makes the paddle keep moving

--- spec/arrowKeyPaddleInputSpec.js
+++ spec/arrowKeyPaddleInputSpec.js
@@ -13,6 +13,57 @@ describe("ArrowKeyPaddleInput", function(){
       document.onkeydown(keyboardEvent);
       expect(paddleController.moveRight).toHaveBeenCalledWith(10);
     });
+    
+    describe("when the user releases and presses the right arrow key again", function(){
+      var rightKeydownEvent;
+      beforeEach(function(){
+        rightKeydownEvent = new KeyboardEvent("keydown", { code: "ArrowRight"});
+      
+        document.onkeydown(rightKeydownEvent);
+        document.onkeyup(new KeyboardEvent("keyup", { code: "ArrowRight"}));
+      });
+      
+      it("moves the paddle right by 10", function(){
+        spyOn(paddleController, "moveRight");  document.onkeydown(rightKeydownEvent);
+        expect(paddleController.moveRight).toHaveBeenCalledWith(10);
+      });
+    });
+    
+    describe("every 15ms", function(){
+      beforeEach(function(){    
+        var keyboardEvent = new KeyboardEvent("keydown", { code: "ArrowRight"});
+      
+        jasmine.clock().install();
+        document.onkeydown(keyboardEvent); 
+      });
+      
+      afterEach(function(){
+        jasmine.clock().uninstall();
+      });
+      
+      it("moves the paddle right again", function(){
+       
+        spyOn(paddleController, "moveRight");
+        
+        jasmine.clock().tick(15);
+        expect(paddleController.moveRight).toHaveBeenCalled();
+        expect(paddleController.moveRight.calls.count()).toBe(1);
+
+        jasmine.clock().tick(15);
+        expect(paddleController.moveRight.calls.count()).toBe(2);
+      });
+    });
+  });
+  
+  describe("when the keydown event repeats", function(){
+    it("does not move the paddle", function(){
+      var keyboardEvent = new KeyboardEvent("keydown", { code: "ArrowRight"});
+      
+      document.onkeydown(keyboardEvent);
+      spyOn(paddleController, "moveRight");
+      document.onkeydown(keyboardEvent);
+      expect(paddleController.moveRight).not.toHaveBeenCalled();
+    });
   });
   
   describe("when the user presses the left arrow key", function(){
--- src/ArrowKeyPaddleInput.js
+++ src/ArrowKeyPaddleInput.js
@@ -1,12 +1,26 @@
 var ArrowKeyPaddleInput = function(options){
   var paddleController = options.paddleController;
+  var inputStates = { NONE: 'none', RIGHT: 'right', LEFT: 'left', LEFT_AND_RIGHT: 'left and right' }
+  var inputState = inputStates.NONE;
+  var movePaddle = function(){
+    paddleController.moveRight(10);
+  };
+  
   var bindInput = function(){
     document.onkeydown = function(keyboardEvent){
-      if(keyboardEvent.code == "ArrowRight"){
-        paddleController.moveRight(10);
-      } else if(keyboardEvent.code == "ArrowLeft") {
-        paddleController.moveLeft(10);
+      if(inputState == inputStates.NONE){
+        if(keyboardEvent.code == "ArrowRight"){
+          paddleController.moveRight(10);
+        } else if(keyboardEvent.code == "ArrowLeft") {
+          paddleController.moveLeft(10);
+        }
+        inputState = inputState.LEFT_AND_RIGHT;
+        setInterval(movePaddle, 15);
       }
+    };
+    
+    document.onkeyup = function(keyboardEvent){
+      inputState = inputStates.NONE;
     };
   }
   bindInput();
