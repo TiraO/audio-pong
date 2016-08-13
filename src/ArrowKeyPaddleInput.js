c0316a53874153ada80ffda7e2ff454980c3cb7e
Tira Odhner <atiraodhner@gmail.com>
2016-08-13 11:10:50 -0400

    rename controlStates to ControlStates

--- src/ArrowKeyPaddleInput.js
+++ src/ArrowKeyPaddleInput.js
@@ -1,11 +1,11 @@
 var ArrowKeyPaddleInput = function(options){
   var paddleController = options.paddleController;
-  var controlStates = { NONE: 'none', RIGHT: 'right', LEFT: 'left' }
-  var controlState = controlStates.NONE;
+  var ControlStates = { NONE: 'none', RIGHT: 'right', LEFT: 'left' }
+  var controlState = ControlStates.NONE;
   var movePaddle = function(){
-    if(controlState == controlStates.RIGHT){
+    if(controlState == ControlStates.RIGHT){
       paddleController.moveRight(10);
-    } else if(controlState == controlStates.LEFT) {
+    } else if(controlState == ControlStates.LEFT) {
       paddleController.moveLeft(10);
     }
   };
@@ -14,20 +14,20 @@ var ArrowKeyPaddleInput = function(options){
     setInterval(movePaddle, 15);
     
     document.onkeydown = function(keyboardEvent){
-      if(controlState == controlStates.NONE){
+      if(controlState == ControlStates.NONE){
         if(keyboardEvent.code == "ArrowRight"){
-          controlState = controlStates.RIGHT;
+          controlState = ControlStates.RIGHT;
         } else if(keyboardEvent.code == "ArrowLeft") {
-          controlState = controlStates.LEFT;
+          controlState = ControlStates.LEFT;
         }
       }
     };
     
     document.onkeyup = function(keyboardEvent){
-      if(keyboardEvent.code == "ArrowLeft" && controlState == controlStates.LEFT){
-        controlState = controlStates.NONE;
-      } else if(keyboardEvent.code == "ArrowRight" && controlState == controlStates.RIGHT){
-        controlState = controlStates.NONE;
+      if(keyboardEvent.code == "ArrowLeft" && controlState == ControlStates.LEFT){
+        controlState = ControlStates.NONE;
+      } else if(keyboardEvent.code == "ArrowRight" && controlState == ControlStates.RIGHT){
+        controlState = ControlStates.NONE;
       }
     };
   }
