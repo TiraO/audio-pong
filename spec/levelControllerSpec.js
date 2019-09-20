b97b349364bc9b21e4e9575745be9acb37459239
Moshe Bildner and Tira Odhner <atiraodhner@gmail.com>
2019-09-20 19:23:01 -0400

    Wip level controller tests

--- spec/levelControllerSpec.js
+++ spec/levelControllerSpec.js
@@ -1,15 +1,43 @@
-xdescribe("LevelController", function(){
+describe("LevelController", function(){
+  var levelController;
+  beforeEach(function(){
+    levelController = new LevelController();
+  });
+
   describe("startLevel", function(){
+    var firstLevelBlocks;
     beforeEach(function(){
-      var levels = [{ 
+      firstLevelBlocks = [ new Block(), new Block()];
+      var levels = [
+      {
+        title: "Take it Block",
+        blocks: firstLevelBlocks
+      },
+      {
         title: "Block Magic, Site Blocker, Blocked and Loaded, Blockout, I'll Be Block, Block to the Future, Microblocking, Site Blocker, Popup Blocker, Communist Block, Block Party, Blockchain, Block is Beautiful, Men in Block, Sunblock, Under Block and Key, Walkie Blockie",
         blocks: []
       }];
     });
     
-    it("sets the level title");
-    it("sticks the ball to the paddle");
-    it("sets the blocks to the blocks for that level");
+    it("sets the level title", function(){
+      levelController.startLevel(0);
+      expect()
+    });
+
+    it("sticks the ball to the paddle", function(){
+        spyOn(singletonContext.ballController, 'stickBallToPaddle');
+        levelController.startLevel();
+        expect(singletonContext.ballController.stickBallToPaddle).toHaveBeenCalled();
+    });
+
+    it("sets the blocks to the blocks for that level", function(){
+      var originalBlocks = singletonContext.blocks;
+
+      levelController.startLevel();
+
+      expect(singletonContext.blocks).toBe(originalBlocks);
+      expect(singletonContext.blocks).toInclude(firstLevelBlocks);
+    });
   });
   
   describe("failLevel", function(){
